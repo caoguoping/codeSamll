7bc40e6149372d9935187efe10173e8394a662da
caoguoping <350232751@qq.com>
2017-02-21 19:18:47 +0800

    sh

---
+++ mahjong/cp.sh
@@ -0,0 +1,6 @@
+rm -rf /f/gitHome/mahjong/*.*
+ cp -r frameworks/runtime-src/Classes /f/gitHome/mahjong/
+ cp -r cocosstudio /f/gitHome/mahjong/  
+ cp -r Mahjong* /f/gitHome/mahjong/
+ cp -r res  /f/gitHome/mahjong/
+ cp -r src  /f/gitHome/mahjong/
---
+++ mahjong/run.sh
@@ -0,0 +1,4 @@
+cp -r src/* simulator/win32/src/
+cp -r res/* simulator/win32/res/
+./simulator/win32/Mahjong.exe
+
